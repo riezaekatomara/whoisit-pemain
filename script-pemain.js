f14e2b74a8ae42f1de4d39a477f8d1853bd44c30
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2024-12-22 20:17:13 +0700

    Add files via upload

---
+++ script-pemain.js
@@ -0,0 +1,210 @@
+function pemain1() {
+  document.getElementById("objek1").src = "a.jpg";
+}
+
+function pemain2() {
+  document.getElementById("objek2").src = "a,jpg";
+}
+
+function pemain3() {
+  document.getElementById("objek3").src = "a,jpg";
+}
+
+function pemain4() {
+  document.getElementById("objek4").src = "a,jpg";
+}
+
+function pemain5() {
+  document.getElementById("objek5").src = "a,jpg";
+}
+
+function pemain6() {
+  document.getElementById("objek6").src = "b.jpg";
+}
+
+function pemain7() {
+  document.getElementById("objek7").src = "b.jpg";
+}
+
+function pemain8() {
+  document.getElementById("objek8").src = "b.jpg";
+}
+
+function pemain9() {
+  document.getElementById("objek9").src = "b.jpg";
+}
+
+function pemain10() {
+  document.getElementById("objek10").src = "b.jpg";
+}
+
+function pemain11() {
+  document.getElementById("objek11").src = "c.jpg";
+}
+
+function pemain12() {
+  document.getElementById("objek12").src = "c.jpg";
+}
+
+function pemain13() {
+  document.getElementById("objek13").src = "c.jpg";
+}
+
+function pemain14() {
+  document.getElementById("objek14").src = "c.jpg";
+}
+
+function pemain15() {
+  document.getElementById("objek15").src = "c.jpg";
+}
+
+function pemain16() {
+  document.getElementById("objek16").src = "d.jpg";
+}
+
+function pemain17() {
+  document.getElementById("objek17").src = "d.jpg";
+}
+
+function pemain18() {
+  document.getElementById("objek18").src = "d.jpg";
+}
+
+function pemain19() {
+  document.getElementById("objek19").src = "d.jpg";
+}
+
+function pemain20() {
+  document.getElementById("objek20").src = "d.jpg";
+}
+function pemain21() {
+  document.getElementById("objek21").src = "e.jpg";
+}
+function pemain22() {
+  document.getElementById("objek22").src = "e.jpg";
+}
+function pemain23() {
+  document.getElementById("objek23").src = "e.jpg";
+}
+function pemain24() {
+  document.getElementById("objek24").src = "e.jpg";
+}
+function pemain25() {
+  document.getElementById("objek25").src = "e.jpg";
+}
+function pemain26() {
+  document.getElementById("objek26").src = "f.jpg";
+}
+function pemain27() {
+  document.getElementById("objek27").src = "f.jpg";
+}
+function pemain28() {
+  document.getElementById("objek28").src = "f.jpg";
+}
+function pemain29() {
+  document.getElementById("objek29").src = "f.jpg";
+}
+function pemain30() {
+  document.getElementById("objek30").src = "f.jpg";
+}
+
+function acak() {
+  let tombol = document.getElementById("tombol");
+  let hasil1 = document.getElementById("hasil1");
+  let hasil2 = document.getElementById("hasil2");
+  hasil2.innerHTML = Math.floor(Math.random() * 35) + 1;
+  {
+    if (hasil2.innerHTML == 1) {
+      hasil2.innerHTML = "Bellingham";
+      hasil1.src = "belingham.jpg";
+    } else if (hasil2.innerHTML == 2) {
+      hasil2.innerHTML = "Bruno";
+      hasil1.src = "bruno.jpg";
+    } else if (hasil2.innerHTML == 3) {
+      hasil2.innerHTML = "Cole Palmer";
+      hasil1.src = "colepalmer.jpg";
+    } else if (hasil2.innerHTML == 4) {
+      hasil2.innerHTML = "Cubarsi";
+      hasil1.src = "cubarsi.jpg";
+    } else if (hasil2.innerHTML == 5) {
+      hasil2.innerHTML = "de Bruyne";
+      hasil1.src = "debruyne.jpg";
+    } else if (hasil2.innerHTML == 6) {
+      hasil2.innerHTML = "Enzo";
+      hasil1.src = "enzo.jpg";
+    } else if (hasil2.innerHTML == 7) {
+      hasil2.innerHTML = "Foden";
+      hasil1.src = "foden.jpg";
+    } else if (hasil2.innerHTML == 8) {
+      hasil2.innerHTML = "Gvardiol";
+      hasil1.src = "gvardiol.jpg";
+    } else if (hasil2.innerHTML == 9) {
+      hasil2.innerHTML = "Halland";
+      hasil1.src = "halland.jpg";
+    } else if (hasil2.innerHTML == 10) {
+      hasil2.innerHTML = "Kane";
+      hasil1.src = "kane.jpg";
+    } else if (hasil2.innerHTML == 11) {
+      hasil2.innerHTML = "Lautaro";
+      hasil1.src = "lautaro.jpg";
+    } else if (hasil2.innerHTML == 12) {
+      hasil2.innerHTML = "Lewandowsky";
+      hasil1.src = "lewy.jpg";
+    } else if (hasil2.innerHTML == 13) {
+      hasil2.innerHTML = "Luis Diaz";
+      hasil1.src = "luisdiaz.jpg";
+    } else if (hasil2.innerHTML == 14) {
+      hasil2.innerHTML = "Mac Allister";
+      hasil1.src = "macalister.jpg";
+    } else if (hasil2.innerHTML == 15) {
+      hasil2.innerHTML = "Mbappe";
+      hasil1.src = "mbappe.jpg";
+    } else if (hasil2.innerHTML == 16) {
+      hasil2.innerHTML = "Musiala";
+      hasil1.src = "musiala.jpg";
+    } else if (hasil2.innerHTML == 17) {
+      hasil2.innerHTML = "Nunez";
+      hasil1.src = "nunez.jpg";
+    } else if (hasil2.innerHTML == 18) {
+      hasil2.innerHTML = "Raphinha";
+      hasil1.src = "raphinha.jpg";
+    } else if (hasil2.innerHTML == 19) {
+      hasil2.innerHTML = "Rice";
+      hasil1.src = "rice.jpg";
+    } else if (hasil2.innerHTML == 20) {
+      hasil2.innerHTML = "Rodri";
+      hasil1.src = "rodri.jpg";
+    } else if (hasil2.innerHTML == 21) {
+      hasil2.innerHTML = "Ruben Dias";
+      hasil1.src = "rubendias.jpg";
+    } else if (hasil2.innerHTML == 22) {
+      hasil2.innerHTML = "Rudiger";
+      hasil1.src = "rudiger.jpg";
+    } else if (hasil2.innerHTML == 23) {
+      hasil2.innerHTML = "Saka";
+      hasil1.src = "saka.jpg";
+    } else if (hasil2.innerHTML == 24) {
+      hasil2.innerHTML = "Salah";
+      hasil1.src = "salah.jpg";
+    } else if (hasil2.innerHTML == 25) {
+      hasil2.innerHTML = "Saliba";
+      hasil1.src = "saliba.jpg";
+    } else if (hasil2.innerHTML == 26) {
+      hasil2.innerHTML = "Son";
+      hasil1.src = "son.jpg";
+    } else if (hasil2.innerHTML == 27) {
+      hasil2.innerHTML = "van Dijk";
+      hasil1.src = "vandijk.jpg";
+    } else if (hasil2.innerHTML == 28) {
+      hasil2.innerHTML = "Vinisius Jr";
+      hasil1.src = "vini.jpg";
+    } else if (hasil2.innerHTML == 29) {
+      hasil2.innerHTML = "Wirtz";
+      hasil1.src = "wirtz.jpg";
+    } else {
+      hasil2.innerHTML = "Yamal";
+      hasil1.src = "yamal.jpg";
+    }
+  }
+  tombol.style.display = "none";
+}
